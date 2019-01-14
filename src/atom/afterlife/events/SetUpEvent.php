0d4084f9c4576ce3b1c26cb6121ca34cc1019c8d
Atomization <stevecoulter99@gmail.com>
2019-01-14 10:13:40 -0600

    init mysql if enabled

--- src/atom/afterlife/events/SetUpEvent.php
+++ src/atom/afterlife/events/SetUpEvent.php
@@ -12,12 +12,12 @@ class SetUpEvent implements Listener {
 
     private $plugin;
     private $player = null;
+    private $database;
 
-
+    private $names = [];
 
     public function __construct($plugin) {
         $this->plugin = $plugin;
-        
     }
 
     public function onJoin(PlayerJoinEvent $event) {
@@ -25,10 +25,33 @@ class SetUpEvent implements Listener {
         $this->player = $player->getName();
 
         $files = scandir($this->plugin->getDataFolder() . "players/");
-        
-        if (!in_array($player->getName().".yml", $files)) {
-            $this->save();
+        if ($this->plugin->config->get('type') === "online") {
+            $this->database = $this->plugin->mysqli;
+            $sql = "SELECT * FROM afterlife";
+            $stmt = mysqli_stmt_init($this->database);
+            $result = mysqli_query($this->database, $sql);
+            $array = [];
+            if (mysqli_num_rows($result) > 0) {
+                while ($row = mysqli_fetch_assoc($result)) {
+                    $array[] = $row;
+                }
+
+                foreach ($array as $data) {
+                    array_push($this->names, $data['name']);
+                }
+
+                if (!in_array($this->player, $this->names)) {
+                    $this->save();
+                }
+            } else {
+                $this->save();
+            }
+        } else {
+            if (!in_array($player->getName().".yml", $files)) {
+                $this->save();
+            }
         }
+        
         $this->setText($this->player);
     }
 
@@ -48,7 +71,13 @@ class SetUpEvent implements Listener {
     }
 
     public function save() {
-        yaml_emit_file($this->getPath(), ["name" => $this->player, "level" => 0, "xp" => 0, "kills" => 0, "deaths" => 0, "kill-streak" => 0, "kill/death-ratio" => 0]);
+        if ($this->plugin->config->get('type') !== "online") {
+            yaml_emit_file($this->getPath(), ["name" => $this->player, "level" => 0, "xp" => 0, "kills" => 0, "deaths" => 0, "kill-streak" => 0, "kill/death-ratio" => 0]);
+        } else {
+            $sql = "INSERT INTO afterlife(name, kills, deaths, ratio, xp, level) VALUES ('$this->player', '0', '0', '0', '0', '0')";
+            mysqli_query($this->database, $sql);
+            array_push($this->names, $this->player);
+        }
     }
 
 }
