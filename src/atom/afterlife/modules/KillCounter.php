5212a07a12cf2f69c6b41c26bb71f4a48916356c
Atomization <stevecoulter99@gmail.com>
2019-01-14 13:39:01 -0600

    Update kill score counting

--- src/atom/afterlife/modules/KillCounter.php
+++ src/atom/afterlife/modules/KillCounter.php
@@ -20,18 +20,42 @@ class KillCounter{
         $this->plugin = $plugin;
         $this->player = $player;
         $path = $this->getPath();
-        if(is_file($path)) {
-            $data = yaml_parse_file($path);
-            $this->data = $data;
-            $this->level = $data["level"];
-            $this->xp = $data["xp"];
-            $this->kills = $data["kills"];
-            $this->deaths = $data["deaths"];
-            $this->killStreak = $data["kill-streak"];
-            $this->ratio = $data["kill/death-ratio"];
-            
+        if ($this->plugin->config->get('type') !== "online") {
+            if(is_file($path)) {
+                $data = yaml_parse_file($path);
+                $this->data = $data;
+                $this->level = $data["level"];
+                $this->xp = $data["xp"];
+                $this->kills = $data["kills"];
+                $this->deaths = $data["deaths"];
+                $this->killStreak = $data["kill-streak"];
+                $this->ratio = $data["kill/death-ratio"];
+            } else {
+                return;
+            }
         } else {
-            return;
+            $sql = "SELECT * FROM afterlife;";
+            $result = mysqli_query($this->plugin->mysqli, $sql);
+            $check = mysqli_num_rows($result);
+            $db = array();
+            $names = array();
+            if ($check > 0) {
+                while ($row = mysqli_fetch_assoc($result)) {
+                    $db[] = $row;
+                }
+                foreach ($db as $kay => $value) {
+                    array_push($names, $value['name']);
+                }
+                if (in_array($this->player, $names)) {
+                    $x = array_search($this->player, $names);
+                    $this->kills = $db[$x]['kills'];
+                    $this->deaths = $db[$x]['deaths'];
+                    $this->ratio = $db[$x]['ratio'];
+                    $this->xp = $db[$x]['xp'];
+                    $this->level = $db[$x]['level'];
+                    $this->killStreak = $db[$x]['streak'];
+                }
+            }
         }
     }
 
@@ -51,7 +75,11 @@ class KillCounter{
     }
 
     public function save() {
-        yaml_emit_file($this->getPath(), ["name" => $this->player, "level" => $this->level, "xp" => $this->xp, "kills" => $this->kills, "deaths" => $this->deaths, "kill-streak" => $this->killStreak, "kill/death-ratio" => $this->ratio]);
+        if ($this->plugin->config->get('type') !== "online") {
+            yaml_emit_file($this->getPath(), ["name" => $this->player, "level" => $this->level, "xp" => $this->xp, "kills" => $this->kills, "deaths" => $this->deaths, "kill-streak" => $this->killStreak, "kill/death-ratio" => $this->ratio]);
+        } else {
+            $sql = "UPDATE afterlife SET kills='$this->kills', streak='$this->killStreak' WHERE name='$this->player'";
+            mysqli_query($this->plugin->mysqli, $sql);
+        }
     }
-
 }
