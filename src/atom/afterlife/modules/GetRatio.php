1d5e23416c2fb81d248fe2fe753e4ad142fb5ac9
Atomization <stevecoulter99@gmail.com>
2019-01-14 22:03:28 -0600

    Retrieve ratio data across network

--- src/atom/afterlife/modules/GetRatio.php
+++ src/atom/afterlife/modules/GetRatio.php
@@ -17,14 +17,36 @@ class GetRatio {
         $this->plugin = $plugin;
         $this->player = $player;
         $path = $this->getPath();
-        if(is_file($path)) {
-            $data = yaml_parse_file($path);
-            $this->data = $data;
-            $this->kills = $data["kills"];
-            $this->deaths = $data["deaths"];
-            
+        if ($this->plugin->config->get('type') !== "online") {
+            if(is_file($path)) {
+                $data = yaml_parse_file($path);
+                $this->data = $data;
+                $this->kills = $data["kills"];
+                $this->deaths = $data["deaths"];
+                $this->ratio = $data["kill/death-ratio"];
+            } else {
+                return;
+            }
         } else {
-            return;
+            $sql = "SELECT * FROM afterlife;";
+            $result = mysqli_query($this->plugin->mysqli, $sql);
+            $check = mysqli_num_rows($result);
+            $db = array();
+            $names = array();
+            if ($check > 0) {
+                while ($row = mysqli_fetch_assoc($result)) {
+                    $db[] = $row;
+                }
+                foreach ($db as $kay => $value) {
+                    array_push($names, $value['name']);
+                }
+                if (in_array($this->player, $names)) {
+                    $x = array_search($this->player, $names);
+                    $this->kills = $db[$x]['kills'];
+                    $this->deaths = $db[$x]['deaths'];
+                    $this->ratio = $db[$x]['ratio'];
+                }
+            }
         }
     }
 
