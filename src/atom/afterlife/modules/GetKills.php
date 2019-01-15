6d3f684731f1a0f625458562c14eb9555e762121
Atomization <stevecoulter99@gmail.com>
2019-01-14 21:59:35 -0600

    Retrieve kills data across network

--- src/atom/afterlife/modules/GetKills.php
+++ src/atom/afterlife/modules/GetKills.php
@@ -15,13 +15,32 @@ class GetKills{
         $this->plugin = $plugin;
         $this->player = $player;
         $path = $this->getPath();
-        if(is_file($path)) {
-            $data = yaml_parse_file($path);
-            $this->data = $data;
-            $this->kills = $data["kills"];
-            
+        if ($this->plugin->config->get('type') !== "online") {
+            if(is_file($path)) {
+                $data = yaml_parse_file($path);
+                $this->data = $data;
+                $this->kills = $data["kills"];
+            } else {
+                return;
+            }
         } else {
-            return;
+            $sql = "SELECT * FROM afterlife;";
+            $result = mysqli_query($this->plugin->mysqli, $sql);
+            $check = mysqli_num_rows($result);
+            $db = array();
+            $names = array();
+            if ($check > 0) {
+                while ($row = mysqli_fetch_assoc($result)) {
+                    $db[] = $row;
+                }
+                foreach ($db as $kay => $value) {
+                    array_push($names, $value['name']);
+                }
+                if (in_array($this->player, $names)) {
+                    $x = array_search($this->player, $names);
+                    $this->kills = $db[$x]['kills'];
+                }
+            }
         }
     }
 
