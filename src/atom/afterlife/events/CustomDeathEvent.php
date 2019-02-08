e211989ae0b1590aef35643e3cad7bafa93ea872
Atomization <stevecoulter99@gmail.com>
2019-02-08 10:16:43 -0600

    Update CustomDeathEvent.php

--- src/atom/afterlife/events/CustomDeathEvent.php
+++ src/atom/afterlife/events/CustomDeathEvent.php
@@ -1,11 +1,25 @@
 <?php
 
+/**
+ *   ____                 _                         _____                          _   
+ *  / ___|  _   _   ___  | |_    ___    _ __ ___   | ____| __   __   ___   _ __   | |_ 
+ * | |     | | | | / __| | __|  / _ \  | '_ ` _ \  |  _|   \ \ / /  / _ \ | '_ \  | __|
+ * | |___  | |_| | \__ \ | |_  | (_) | | | | | | | | |___   \ V /  |  __/ | | | | | |_ 
+ *  \____|  \__,_| |___/  \__|  \___/  |_| |_| |_| |_____|   \_/    \___| |_| |_|  \__|            
+ * 
+ * @author iAtomPlaza
+ * @link https://twitter.com/iAtomPlaza                                                                        
+ */
+
 namespace atom\afterlife\events;
 
+# player instance
 use pocketmine\Player;
-use pocketmine\utils\Config;
+
+# utils
 use pocketmine\utils\TextFormat as color;
-use pocketmine\math\Vector3;
+
+# events
 use pocketmine\event\Listener;
 use pocketmine\event\entity\EntityDamageEvent;
 use pocketmine\event\entity\EntityDamageByEntityEvent;
@@ -34,44 +48,23 @@ class CustomDeathEvent implements Listener {
 
                 if ($event instanceof EntityDamageByEntityEvent) {
                     $killer = $event->getDamager();
-                    $this->plugin->addKill($killer->getName());
-                    $this->plugin->addDeath($victim->getName());
-                    $this->plugin->getServer()->broadcastMessage(color::GRAY.$victim->getName().color::WHITE." Was Killed by ".color::GRAY.$killer->getName());
-                    if ($this->plugin->config->get("use-levels") == true) {
-                        $this->plugin->addXp($killer->getName(), $this->plugin->config->get("add-level-xp-amount"));
-                        $this->plugin->removeXp($victim->getName(), $this->plugin->config->get("loose-level-xp-amount"));
-                        if ($this->plugin->getXp($killer->getName()) > $this->plugin->config->get("xp-levelup-ammount")) {
-                            $this->plugin->addLevel($killer->getName(), $this->plugin->getLevel($killer->getName()) + 1);
-                        }
+                    if ($killer instanceof Player && $victim instanceof Player) {
+                        $this->plugin->getAPI()->addKill($killer->getName());
+                        $this->plugin->getAPI()->addDeath($victim->getName());
+                        $this->plugin->getServer()->broadcastMessage(color::GRAY.$victim->getName().color::WHITE." Was Killed by ".color::GRAY.$killer->getName());
                     }
                 } else {
-                    $this->plugin->addDeath($victim->getName());
-                    if ($this->plugin->config->get("use-levels") == true) {
-                        $this->plugin->removeXp($victim->getName(), $this->plugin->config->get("loose-level-xp-amount"));
+                    /**
+                     * @author TheWalker0
+                     * Fixing a bug that i couldnt reproduce
+                     */
+                    if ($victim instanceof Player) {
+                        $this->plugin->getAPI()->addDeath($victim->getName());
+                        if ($this->plugin->config->get("use-levels") == true) {
+                            $this->plugin->getAPI()->removeXp($victim->getName(), $this->plugin->config->get("loose-level-xp-amount"));
+                        }
                     }
                 }
-                foreach ($this->plugin->getServer()->getOnlinePlayers() as $players) {
-                    /*
-                    sets the floating text 10 times in an attempt 
-                    to overrite the existing text leader boards
-                    as there is no function in pmmp to remove floating text
-
-                    There should be `removeParticle();` but there isnt :(
-                    */
-                    for ($i=0; $i <= 10; $i++) { 
-                        // $this->sendText($players->getName());
-                    }
-                }
-            }
-        }
-    }
-
-    public function sendText(string $name) {
-        foreach($this->plugin->texts->getAll() as $loc => $type) {
-        $pos = explode("_", $loc);
-            if(isset($pos[1])) {
-                $v3 = new Vector3((float) $pos[0],(float) $pos[1],(float) $pos[2]);
-                $this->plugin->addText($v3, $type, [$this->plugin->getServer()->getPlayerExact($name)]);
             }
         }
     }
